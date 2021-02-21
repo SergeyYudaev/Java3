a45d2a20b669d068495fd7db0806660fc54ca83a
SergeyYudaev <grossherz24@gmail.com>
2021-02-21 17:50:16 +0300

    Переделал на выспавшуюся голову

--- chat_geekchat_16012021/server/src/main/java/server/SQLiteAuthService.java
+++ chat_geekchat_16012021/server/src/main/java/server/SQLiteAuthService.java
@@ -4,25 +4,27 @@ import java.sql.*;
 
 public class SQLiteAuthService {
     private static Connection connection;
-    private static Statement statement;
+
 
     public static void connection() throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
         connection = DriverManager.getConnection("jdbc:sqlite:server/myUserList.db");
-        statement = connection.createStatement();
     }
 
 
     //аутентификация
     public static String getNicknameByLoginAndPassword(String login, String password) {
-        String str = ("select * from UserList where login = '" + login + "'");
+        String loginStr = ("select * from UserList where login = ? and password = ?");
 
-        try {
-            ResultSet resultSet = statement.executeQuery(str);
+        try (PreparedStatement psAuth = connection.prepareStatement(loginStr)) {
+            psAuth.setString(1, login);
+            psAuth.setString(2, password);
+            ResultSet resultSet = psAuth.executeQuery();
+
             if (resultSet.next()) {
-                if (resultSet.getString("Password").equals(password))
-                    return resultSet.getString("Nickname");
+                return resultSet.getString("Nickname");
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -32,49 +34,88 @@ public class SQLiteAuthService {
 
     //регистрация
     public static boolean registration(String login, String password, String nickname) {
-        String check = ("select * from UserList where nickname = '" + nickname + "'");
-        String add = String.format("insert into UserList (login, password, nickname) values ('%s', '%s', '%s')", login, password, nickname);
+        String checkLogin = ("select * from UserList where Login = ?");
+        String checkNick = ("select * from UserList where Nickname = ?");
+        String reg = ("insert into UserList (Login, Password, Nickname) values (?, ?, ?)");
 
-        try {
-            ResultSet resultSetCheck = statement.executeQuery(check);
-            if (resultSetCheck.next()) {
-                if (resultSetCheck.getString("Nickname").equals(nickname))
-                    return false;
+        try (PreparedStatement psCheckLogin = connection.prepareStatement(checkLogin)) {
+            psCheckLogin.setString(1, login);
+            ResultSet rsLoginCheck = psCheckLogin.executeQuery();
+
+            if (rsLoginCheck.next()) {
+                return false;
+            } else {
+
+                try (PreparedStatement psCheckNick = connection.prepareStatement(checkNick)) {
+                    psCheckNick.setString(1, nickname);
+                    ResultSet rsCheckNick = psCheckNick.executeQuery();
+
+                    if (rsCheckNick.next()) {
+                        return false;
+                    } else {
+
+                        try (PreparedStatement psRegistration = connection.prepareStatement(reg)) {
+                            psRegistration.setString(1, login);
+                            psRegistration.setString(2, password);
+                            psRegistration.setString(3, nickname);
+                            psRegistration.executeUpdate();
+                            return true;
+                        }
+                    }
+                }
             }
-            ResultSet resultSetAdd = statement.executeQuery(add);
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        return true;
-    }
-
-
-    private static void print(ResultSet resultSet) throws SQLException {
-        while (resultSet.next()) {
-            System.out.print(resultSet.getString("Login") + " ");
-            System.out.print(resultSet.getString("Password") + " ");
-            System.out.print(resultSet.getString("Nickname") + "\n");
-        }
+        return false;
     }
 
     //смена ника
-    public static boolean changeNick(String oldNick, String login, String password, String nickname) {
-        String check = ("select * from UserList where login = '" + login + "'");
-        String change = String.format("update UserList set Nickname = '%s' where Login = '%s'", nickname, login);
+    public static boolean changeNick(String login, String password, String nickname) {
+        String checkLogin = ("select * from UserList where Login = ? and password =?");
+        String checkNick = ("select * from UserList where Nickname = ?");
+        String change = ("update UserList set Nickname = ? where Login = ?");
 
-        try {
-            ResultSet resultSetCheck = statement.executeQuery(check);
-            if (resultSetCheck.next()) {
-                if (resultSetCheck.getString("password").equals(password)) {
-                    ResultSet resultSetChange = statement.executeQuery(change);
+
+        try (PreparedStatement psCheck = connection.prepareStatement(checkLogin);) {
+            psCheck.setString(1, login);
+            psCheck.setString(2, password);
+            ResultSet rsCheckLogin = psCheck.executeQuery();
+
+            if (rsCheckLogin.next()) {
+                System.out.println("login and pass ok");
+
+                try (PreparedStatement isNickAvailable = connection.prepareStatement(checkNick)) {
+                    isNickAvailable.setString(1, nickname);
+                    ResultSet rsCheckNick = isNickAvailable.executeQuery();
+
+                    if (rsCheckNick.next()) {
+                        System.out.println("ник занят");
+                        return false;
+
+                    } else {
+
+                        try (PreparedStatement changeNick = connection.prepareStatement(change)) {
+                            System.out.println("Цикл смены ника");
+                            changeNick.setString(1, nickname);
+                            changeNick.setString(2, login);
+                            changeNick.executeUpdate();
+                            System.out.println("ник должен смениться");
+                            return true;
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
                 }
             }
-            return false;
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return true;
+        return false;
     }
 
 }
